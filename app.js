30c572ee4be95d4b5b2c9c69d2a6228f9955aeba
deadfantom2 <cameratest81@gmail.com>
2017-11-02 10:53:15 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express             = require('express');
+
+
+
+var app = express();
+
+
+
+
+app.get('/', function (req, res) {
+   res.send({name: "lexx"});
+});
+
+
+// Start Server
+var server = app.listen(8000, function(){
+    console.log('Server started on port 8000');
+});
